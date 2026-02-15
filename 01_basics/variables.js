bc3be1854bf29c3581aa42bf14dbcc316ff41994
mijbahur <hacinrahman03@gmail.com>
2026-02-16 03:06:47 +0600

    variables and data type

---
+++ 01_basics/variables.js
@@ -0,0 +1,40 @@
+const accountId = 6233;
+let eamilId = "hacin@gamil.com";
+var password = "fdsfds";
+
+fullName = "Hacin Mijbahur Rahman";
+
+// accountId = 43278 // it is not allowed
+
+eamilId = "mijbahur@gamil.com"
+password = 234987
+fullName = "hacin"
+
+console.table([accountId, eamilId, password, fullName]);
+
+// refer not to use var because of issue in block scope and functional scope 
+
+
+// data type
+
+"use strict"; // it will treat all JS code as newer version
+
+// alert (42-32) // it will not work in vs code. we are using nodejs, not browser
+
+// Number -> 2^53
+// BigInt
+// String -> " "
+// Boolean -> true/false
+// null -> standalone value // null is a object
+// undefined -> variable is not defiend
+// Symbol -> unique // details in react
+// Object
+
+console.log("");
+console.log("-------Data type-------");
+
+console.log(typeof accountId); // number
+console.log(typeof "hacin"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undifiend
+
